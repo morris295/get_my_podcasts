656932c3af2a8ca14a6424044ae62ae263817ed9
Jesse Morris <jdm.github@gmail.com>
2016-05-27 12:59:56 -0400

    Fixing issue with podcast artwork on index page.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,26 +19,26 @@
         </form>
 	</div>
 	<div class="col-md-6">
-		<table class="table table-responsive">
+		<table class="table table-responsive shows-table">
 			<tr>
 				<th colspan="2">Top shows yesterday</th>
 			</tr>
 			@foreach ($topShows as $show)
 				<tr>
-					<td><a href="shows/{{$show['as_id']}}"><img src="{{$show['image_url']}}"></a></td>
+					<td class="artwork"><a href="shows/{{$show['as_id']}}"><img class="img-responsive" src="{{$show['image_url']}}"></a></td>
 					<td><a href="shows/{{$show['as_id']}}">{{$show["title"]}}</a></td>
 				</tr>
 			@endforeach
 		</table>
 	</div>
 	<div class="col-md-6">
-		<table class="table table-responsive">
+		<table class="table table-responsive shows-table">
 			<tr>
 				<th colspan="2">Most Popular</th>
 			</tr>
 			@foreach ($tastemakers as $show)
 				<tr>
-					<td><a href="shows/{{$show['as_id']}}"><img src="{{$show['image_url']}}"></a></td>
+					<td class="artwork"><a href="shows/{{$show['as_id']}}"><img class="img-responsive" src="{{$show['image_url']}}"></a></td>
 					<td><a href="shows/{{$show['as_id']}}">{{$show["title"]}}</a></td>
 				</tr>
 			@endforeach
