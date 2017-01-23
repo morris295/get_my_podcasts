d3423dbc55d72a1d6d3ede46fb8747a69393db69
Jesse Morris <jdm.github@gmail.com>
2017-01-22 21:10:16 -0500

    Additional changes for UI update and new features.

---
+++ public/js/playlist.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	setTimeout(function() {
+		loader.stop();
+	}, 3000);
+});
---
+++ public/js/search.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	loader = new Loader();
+	loader.stop();
+	$("header").fadeIn("slow");
+	$("section").fadeIn("slow");
+	$("aside").fadeIn("slow");
+});
